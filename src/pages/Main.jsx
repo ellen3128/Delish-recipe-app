83f8ebc6a3f2c13df4297c7cd36fc3f0ee60bb53
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-05 15:43:44 -0400

    updates cuisine and dessert jsx starter files and routes in Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import Dessert from "./Dessert";
 
 function pages() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+        <Route path="/dessert" element={<Dessert />} />
       </Routes>
     </Router>
   );
