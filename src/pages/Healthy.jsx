bd3d0a823e64487f58538a84d254c63437296f88
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-06 09:44:51 -0400

    updates healthy images rendering and basic page layout

--- src/pages/Healthy.jsx
+++ src/pages/Healthy.jsx
@@ -10,26 +10,27 @@ export default function Healthy() {
 
   const getHealthy = async () => {
       const api = await fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=9&diet=Ketogenic|GlutenFree|Vegetarian`
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=12&diet=Ketogenic|GlutenFree|Vegetarian`
       );
       const data = await api.json();
       console.log(data);
-      // Make sure 'data.recipes' is the correct property
+      // Make sure 'data.recipes' is the correct property => returned undefined bc it's an object
       setHealthy(data.results);
   };
 
   return (
     <div>
         <h3>Healthy Selections</h3>
+        <div className="grid-container">
         {healthy.map((recipe) => (
           <div className="card-healthy" key={recipe.id}>
             <div className="img-container">
               <img src={recipe.image} alt={recipe.title} />
-              <div className="gradient"></div>
             </div>
             <h4>{recipe.title}</h4>
-          </div>
+            </div>
         ))}
+      </div>
       </div>
   );
 }
