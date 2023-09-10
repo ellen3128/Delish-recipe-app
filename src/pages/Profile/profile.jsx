204d282dd5556b5809b8ab44da83b9c9939f4d05
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-10 16:53:05 -0400

    updates profile page with css styling

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router";
-import './Profile.css';
+import "./Profile.css";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -19,10 +19,14 @@ const Profile = () => {
   // If the user is authenticated and loading is complete, render the user's profile data
   return (
     <div className="container-user">
-        
+      <div className="image-container">
         <img className="userpic" src={user.picture} alt={""} />
-        <h2 className="userName">Welcome, <span style={{color:"#00473D", fontWeight:"800"}}>{user.name}</span></h2>
-        <p className="userEmail">{user.email}</p>
+      </div>
+      <h2 className="userName">
+        Welcome,{" "}
+        <span style={{ color: "#00473D", fontWeight: "800" }}>{user.name}</span>
+      </h2>
+      <p className="userEmail">{user.email}</p>
       {/* You can add more user details here, such as user.email, user.picture, etc. */}
     </div>
   );
