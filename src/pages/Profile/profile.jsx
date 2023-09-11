b7f6d1c8753592007e6980940e41d81a53e1cbce
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-11 09:38:57 -0400

    adds favorites functionality on desserts and profiles page

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-import { Navigate } from "react-router";
+import { Navigate, Link } from "react-router-dom";
 import "./Profile.css";
 
-const Profile = () => {
+export default function Profile () {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const [favoriteDesserts, setFavoriteDesserts] = useState([]);
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favoriteDesserts') || '[]');
+
+    if (storedFavorites.length) {
+      const fetchFavorites = async () => {
+        try {
+          const api = await fetch(
+            `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_API_KEY}&ids=${storedFavorites.join(',')}`
+          );
+          const data = await api.json();
+          setFavoriteDesserts(data);
+        } catch (error) {
+          console.error("Error fetching favorite dessert details:", error);
+        }
+      };
+      
+      fetchFavorites();
+    }
+  }, []);
+
 
   // If user is not authenticated, redirect to home route
   if (!isAuthenticated) {
@@ -19,17 +42,31 @@ const Profile = () => {
   // If the user is authenticated and loading is complete, render the user's profile data
   return (
     <div className="container-user">
-      <p className="image-container">
+      <div className="image-container">
         <img className="userpic" src={user.picture} alt={""} />
-      </p>
+      </div>
       <h2 className="userName">
         Welcome,{" "}
         <span style={{ color: "#00473D", fontWeight: "800" }}>{user.name}</span>
       </h2>
       <p className="userEmail">{user.email}</p>
       {/* You can add more user details here, such as user.email, user.picture, etc. */}
+    
+      {favoriteDesserts.length > 0 && (
+        <div>
+          <h3>Your Favorite Desserts:</h3>
+          <ul>
+            {favoriteDesserts.map(dessert => (
+              <li key={dessert.id}>
+                {dessert.title}
+                <Link to={`/recipe/${dessert.id}`}>{dessert.title}</Link>
+                </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Profile;
+
