4aff6d2709c580c55f6f2a91ec585b56067eed6e
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-08 00:34:51 -0500

    adds media query to banner section

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 function Banner() {
   return (
     <div className="grid grid-cols-2">
-        <img src={bannerImage} alt="ingredientsImg"></img>
+        <img src={bannerImage} alt="ingredientsImg" className="bannerImg"></img>
         <div className="text"> <p>A DELISH life <br/> begins here.</p> <br>
         </br> <p className="subtext"> Find a well-balanced meal that is both healthy and delicious!</p> </div>
     </div>
