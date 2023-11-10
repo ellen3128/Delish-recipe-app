51d5ebbef8e24489ed9d68f71acba6a352d14ca2
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-09 19:58:56 -0500

    adds media query to cuisine category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -9,20 +9,20 @@ function Category() {
   return (
     <div className="list">
       <NavLink className="slink" to={'/cuisines/Italian'} activeclassname="active">
-        <FaPizzaSlice />
-        <h4>Italian</h4>
+        <FaPizzaSlice className="category-icon" />
+        <h4 className="category-name">Italian</h4>
       </NavLink>
       <NavLink className="slink" to={'/cuisines/American'} activeclassname="active">
-        <FaHamburger />
-        <h4>American</h4>
+        <FaHamburger className="category-icon" />
+        <h4 className="category-name">American</h4>
       </NavLink>
       <NavLink className="slink" to={'/cuisines/Thai'} activeclassname="active">
-        <GiNoodles />
-        <h4>Thai</h4>
+        <GiNoodles className="category-icon" />
+        <h4 className="category-name">Thai</h4>
       </NavLink>
       <NavLink className="slink" to={'/cuisines/Japaneses'} activeclassname="active">
-        <GiChopsticks />
-        <h4>Japanese</h4>
+        <GiChopsticks className="category-icon" />
+        <h4 className="category-name">Japanese</h4>
       </NavLink>
     </div>
   )
