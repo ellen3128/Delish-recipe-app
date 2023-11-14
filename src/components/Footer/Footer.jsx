be004375e1b839d4233270d577dda4db03b6fa96
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-13 23:08:56 -0500

    adds media query to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
+import './Footer.css';
 
-function Footer() {
+export default function Footer() {
   return (
-    <div style={{backgroundColor: "#00483E"}}>
-    <div style={{ borderTop: '1px solid #808080', marginTop: "2rem"}}>
-      <p style={{ color:"#F4F3E7", letterSpacing:"2px", textAlign: "center", padding: "2rem", fontSize: "1rem" }}>
-        &copy; 2023 Ellen Park - All rights reserved.
-      </p>
-    </div>
+    <div className="footer">
+      <p>&copy; 2023 Ellen Park - All rights reserved.</p>
     </div>
   );
 }
 
-export default Footer;
