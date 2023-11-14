8919412ad49df92a98fd2bc1cf82814635bd809f
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-14 10:39:00 -0500

    adjusts media query

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -4,10 +4,12 @@ import './Header.css';
 
 function Banner() {
   return (
+    <div className="bannerContainer">
     <div className="grid grid-cols-2">
         <img src={bannerImage} alt="ingredientsImg" className="bannerImg"></img>
         <div className="text"> <p>A DELISH life <br/> begins here.</p> <br>
         </br> <p className="subtext"> Find a well-balanced meal that is both healthy and delicious!</p> </div>
+        </div>
     </div>
   )
 }
