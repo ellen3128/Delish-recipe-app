accf9f3a137c4ad9d49b168a8c595e5f1ea9b7df
ellen3128 <hyunpark.ellen@gmail.com>
2023-11-13 22:04:38 -0500

    restructures splide gap as useState hook

--- src/components/OurPicks/Veggies.jsx
+++ src/components/OurPicks/Veggies.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
